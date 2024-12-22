347653c5150d5ff3877ffe193dadf5c0e215e0a6
Manuel <72426487+MAGE-VOID@users.noreply.github.com>
2024-12-21 23:34:13 -0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,15 @@
 import React, { Suspense } from "react";
 import Layout from "@/components/Layout/Layout";
 const Hero = React.lazy(() => import("@/components/HeroModule/Hero"));
-const Section_1 = React.lazy(() => import("@/components/Sections/Section_1/Section_1"));
-const Section_2 = React.lazy(() => import("@/components/Sections/Section_2/Section_2"));
-const Section_3 = React.lazy(() => import("@/components/Sections/Section_3/Section_3"));
+const Section_1 = React.lazy(
+  () => import("@/components/Sections/Section_1/Section_1")
+);
+const Section_2 = React.lazy(
+  () => import("@/components/Sections/Section_2/Section_2")
+);
+const Section_3 = React.lazy(
+  () => import("@/components/Sections/Section_3/Section_3")
+);
 
 export default function Home() {
   return (
